aeeecf310edb8c5c13854be581449f0e597dbb00
Superkrypt0 <kryptomolokwe@gmail.com>
2020-01-06 11:55:57 +0200

    Add files via upload

--- core/forms.py
+++ core/forms.py
@@ -1,11 +1,10 @@
-from django import forms
+from django import forms, ModelForm
 from django.contrib.auth.models import User
-from .models import SingleRoom
-
+from .models import Room, Reservation
 
 class SingleRoomForm(forms.ModelForm):
 	class Meta:
-		model = SingleRoom
+		model = Room
 		fields= [
 			"name",
 			"surname",
@@ -13,8 +12,49 @@ class SingleRoomForm(forms.ModelForm):
 			"arrival_date",
 			"departure_date",
 		]
-		
-            
-    
+		widgets = {
+			'arrival_date': forms.SelectDateWidget(),
+			'departure_date': forms.SelectDateWidget(),
+		}
 
+	
+class RoomForm(forms.ModelForm):
+	class Meta:
+		model = Room
+		fields= [
+			"name",
+			"surname",
+			"email",
+			"arrival_date",
+			"departure_date",
+		] 
+
+		widgets = {
+			'arrival_date': forms.SelectDateWidget(),
+			'departure_date': forms.SelectDateWidget(),
+		}    
+
+    
+class CheckAvailability(forms.ModelForm):
+	class Meta:
+		model = Room
+		fields = [
+			"room_type",
+			"arrival_date",
+			"departure_date",
+		]
+		widgets = {
+			'arrival_date': forms.SelectDateWidget(),
+			'departure_date': forms.SelectDateWidget(),
+		}
 		
+		
+class ConfirmForm(object):
+	class Meta:
+		model = Reservation
+		fields = [
+			" check_in",
+			" check_out",
+		]
+
+						
